b3502cfa3dd68b97e8de20fa337cc02f3f3e6903
Kaixi Zhang <zhangkaixi25mail@gmail.com>
2022-09-07 02:26:45 +0000

    Update index.html and App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  Hello Vue 3 in CodeSandbox!
+  <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
 </template>
 
 <script>
+import HelloWorldVue from "./components/HelloWorld.vue";
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+  },
 };
 </script>
 
 <style>
 #app {
   text-align: center;
+}
+</style>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
