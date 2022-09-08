b3925d4562944327b175774f6ff7d3d4124c9a7b
zhangkaixi <17562065091@163.com>
2022-09-08 22:10:40 +0800

    commit

--- src/views/account/login.vue
+++ src/views/account/login.vue
@@ -1,209 +1,147 @@
-<!-- eslint-disable no-unused-vars -->
 <template>
-    <div>
-        <div @contextmenu.stop="" id="bubble" class="bubble">
-            <canvas id="bubble-canvas" class="bubble-canvas"></canvas>
-        </div>
-        <div class="login">
-            <div class="login-box">
-                <div class="head">
-                    <img src="../../assets/login-header.png" alt="" />
-                </div>
-                <div class="form">
-                    <img class="profile-avatar" src="../../assets/avatar.png" alt="" />
-                    <div class="content">
-                        <el-form @keyup.enter="onSubmit(formRef)" ref="formRef" :rules="rules" size="large"
-                            :model="form">
-                            <el-form-item prop="username">
-                                <el-input ref="usernameRef" type="text" clearable v-model="form.username"
-                                    :placeholder="请输入账号">
-                                    <template #prefix>
-                                        <Icon name="fa fa-user" class="form-item-icon" size="16"
-                                            color="var(--el-input-icon-color)" />
-                                    </template>
-                                </el-input>
-                            </el-form-item>
-                            <el-form-item prop="password">
-                                <el-input ref="passwordRef" v-model="form.password" type="password" :placeholder="请输入密码"
-                                    show-password>
-                                    <template #prefix>
-                                        <Icon name="fa fa-unlock-alt" class="form-item-icon" size="16"
-                                            color="var(--el-input-icon-color)" />
-                                    </template>
-                                </el-input>
-                            </el-form-item>
-                            <el-form-item>
-                                <el-button :loading="form.loading" class="submit-button" round type="primary"
-                                    size="large" @click="onSubmit(formRef)">
-                                    登录
-                                </el-button>
-                            </el-form-item>
-                        </el-form>
-                    </div>
-                </div>
+    <el-row class="min-h-screen">
+        <el-col :lg="9" :md="12" class="admin-login-left">
+            <h2 class="admin-login-title">欢迎回来</h2>
+            <div class="admin-login-info">
+                <span class="admin-line"></span>
+                <span>账号密码登录</span>
+                <span class="admin-line"></span>
             </div>
-        </div>
-    </div>
+            <el-form ref="loginForm" :model="form" class="w-[250px]" :rules="rules">
+                <el-form-item prop="username">
+                    <el-input v-model.trim="form.username" type="text" maxlength="20" minlength="3"
+                        placeholder="请输入用户名">
+                        <template #prefix>
+                            <el-icon>
+                                <user />
+                            </el-icon>
+                        </template>
+                    </el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input v-model.trim="form.password" @keydown.enter="handleLoginSubmit" type="password"
+                        maxlength="20" minlength="3" placeholder="请输入密码">
+                        <template #prefix>
+                            <el-icon>
+                                <lock />
+                            </el-icon>
+                        </template>
+                    </el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button round color="#626aef" class="w-[250px]" type="primary" :loading="loading"
+                        @click="handleLoginSubmit">登 录
+                    </el-button>
+                </el-form-item>
+                <el-form-item>
+                    <el-button round color="#626aef" class="w-[250px]" type="primary" :loading="loading"
+                        @click="handleDingTalkLogin">钉钉登录
+                    </el-button>
+                </el-form-item>
+            </el-form>
+        </el-col>
+        <el-col :lg="15" :md="12" class="admin-login-right">
+            <div class="text-center">
+                <img src="@/assets/img/hero_img.png" alt="">
+                <div class="font-bold text-5xl text-light-50 mb-4 mt-9">欢迎光临</div>
+            </div>
+        </el-col>
+    </el-row>
 </template>
-
+  
 <script setup>
-import { reactive } from 'vue';
-import { ElForm, ElInput } from 'element-plus'
-import { ElNotification } from 'element-plus'
-import { validatorAccount, validatorPassword } from '../../utils/validate.js'
+import { reactive, ref } from 'vue'
+// eslint-disable-next-line no-unused-vars
+import store from "@/store";
+import router from "@/router";
 
+// eslint-disable-next-line no-unused-vars
 const form = reactive({
-    username: '',
-    password: '',
-    captcha: '',
-    keep: false,
-    loading: false,
-    captcha_id: '',
+    username: 'admin@admin.cc',
+    password: '123456'
 })
+
+// 定义属性
+// eslint-disable-next-line no-unused-vars
+const loading = ref(false);
+
+// 表单验证
+const loginForm = ref(null);
 
 // 表单验证规则
-const rules = reactive({
+// eslint-disable-next-line no-unused-vars
+const rules = {
     username: [
-        {
-            required: true,
-            message: '请输入账号',
-            trigger: 'blur',
-        },
-        {
-            validator: validatorAccount,
-            trigger: 'blur',
-        },
+        { required: true, message: '请输入用户名', trigger: 'blur' },
+        { min: 4, max: 20, message: '你的用户名必须是 4 to 20', trigger: 'blur' }
     ],
     password: [
-        {
-            required: true,
-            message: '请输入密码',
-            trigger: 'blur',
-        },
-        {
-            validator: validatorPassword,
-            trigger: 'blur',
-        },
-    ],
-})
+        { required: true, message: '请输入密码', trigger: 'blur' },
+        { min: 4, max: 20, message: '你的密码必须是 4 to 20', trigger: 'blur' }
+    ]
+};
 
-// const usernameRef = ref(null)
-// const passwordRef = ref(null)
-
-// const focusInput = () => {
-//     if (form.username === '') {
-//         usernameRef.value!.focus()
-//     } else if (form.password === '') {
-//         passwordRef.value!.focus()
-//     }
-// }
-
-const onSubmit = (formEl) => {
-    if (!formEl) return
-    formEl.validate((valid) => {
-        if (valid) {
-            form.loading = true
-            // login('post', form)
-            //     .then((res) => {
-            form.loading = false
-            // adminInfo.dataFill(res.data.userinfo)
-            ElNotification({
-                message: '登录成功',
-                type: 'success',
-            })
-            // router.push({ path: res.data.routePath })
-            // })
-            // .catch(() => {
-            //     onChangeCaptcha()
-            //     form.loading = false
-            // })
-        } else {
-            // onChangeCaptcha()
-            return false
+// 表单提交
+// eslint-disable-next-line no-unused-vars
+const handleLoginSubmit = () => {
+    // 1: 提交表单校验 校验表单输入
+    loginForm.value.validate(async (valid) => {
+        // 如果为valid = false 还存在非法数据.
+        if (!valid) {
+            return;
         }
-    })
+        try {
+            // form.password = encryptByDES(form.password)
+            // store.dispatch("user/login", form).then(() => {
+            router.push("/Home")
+            // });
+        } catch (e) {
+            console.log(e);
+        }
+    });
+};
+
+// eslint-disable-next-line no-unused-vars
+const handleDingTalkLogin = () => {
+
 }
+
 </script>
-
+  
 <style scoped>
-.bubble {
-    overflow: hidden;
-    background: url(/@/assets/bg.jpg) repeat;
-}
-
-.form-item-icon {
-    height: auto;
-}
-
-.login {
-    position: absolute;
-    top: 0;
+.admin-login-left {
+    background-color: rgba(253, 253, 253, 1);
     display: flex;
-    width: 100vw;
-    height: 100vh;
-    align-items: flex-start;
+    align-items: center;
     justify-content: center;
+    flex-direction: column;
+    background: url('/src/assets/img/banner.png');
+    background-size: cover;
 }
 
-.login .login-box {
-    overflow: hidden;
-    width: 430px;
-    padding: 0;
-    background: var(--ba-bg-color-overlay);
-    margin-top: 130px;
+.admin-login-title {
+    font-weight: 700;
+    font-size: 1.875rem;
+    line-height: 2.25rem;
+    color: rgba(31, 41, 55, 1);
 }
 
-.login .head {
-    background: #ccccff;
+.admin-login-info {
+    @apply flex items-center justify-center my-5 text-gray-300 space-x-2;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 1.25rem;
+    margin-bottom: 1.25rem;
+    color: rgba(209, 213, 219, 1);
+
 }
 
-.login .head img {
-    display: block;
-    width: 430px;
-    margin: 0 auto;
-    user-select: none;
+.admin-line {
+    @apply h- [ 1px] w-16 bg-gray-200;
 }
 
-.login .form {
-    position: relative;
-}
-
-.login .form .profile-avatar {
-    display: block;
-    position: absolute;
-    height: 100px;
-    width: 100px;
-    border-radius: 50%;
-    border: 4px solid var(--ba-bg-color-overlay);
-    top: -50px;
-    right: 175px;
-    z-index: 2;
-    user-select: none;
-}
-
-.login .form .content {
-    padding: 100px 40px 40px 40px;
-}
-
-.login .form .submit-button {
-    width: 100%;
-    letter-spacing: 2px;
-    font-weight: 300;
-    margin-top: 15px;
-    --el-button-bg-color: var(--el-color-primary);
-}
-
-@media screen and (max-width: 720px) {
-    .login {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-
-    }
-
-    .login .login-box {
-        width: 340px;
-        margin-top: 0;
-    }
+.admin-login-right {
+    @apply flex items-center justify-center bg-indigo-500;
 }
 </style>
